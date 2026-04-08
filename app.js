47f2dfdca6aca5e297ccc9e068e51f45b8c7dffc
piecahcih <pichayapa.thai@gmail.com>
2026-04-08 14:13:16 +0700

    peachja

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to api!");
 });
+app.get("/users", (req, res) => res.send("User list"));
+app.get("/benjy", (req, res) => res.send("benjy"));
+app.get("/peach", (req, res) => res.send("peach"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
